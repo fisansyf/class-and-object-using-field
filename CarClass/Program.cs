27573521d20e6a4b6bbacbd85b6f78ada1d70a52
fisansyf <fisansyafa812@students.amikom.ac.id>
2023-04-03 22:54:14 +0700

    tugas praktikum4

---
+++ CarClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            car mobil1 = new car();
+
+            mobil1.Warna = "Hitam";
+            mobil1.JumlahPintu = 4;
+            mobil1.Merk = "TOYOTA";
+            mobil1.Model = "CAMRY";
+            mobil1.TahunKeluar = 2022;
+
+            mobil1.Gas(150);
+            mobil1.Klakson("tiiinn...tiiinn");
+            mobil1.Tampilkaninfo();
+
+
+
+            Console.Write("Press C to close this program ");
+            while (Console.ReadKey().Key != ConsoleKey.C) { }
+        }
+    }
+}
---
+++ CarClass/car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    internal class car
+    {
+        //field
+        private string _warna;
+        private int _jumlahpintu;
+        private string _merk;
+        private string _model;
+        private int _tahunkeluar;
+
+        //properties
+        public string Warna 
+        {
+            get
+            {
+                return _warna;
+            }
+            set
+            {
+                _warna = value;
+            }
+        }
+        public int JumlahPintu
+        {
+            get 
+            { 
+                return _jumlahpintu; 
+            }
+            set 
+            {
+                _jumlahpintu = value;
+            }
+        }
+        public string Merk
+        {
+            get
+            {
+                return _merk;
+            }
+            set
+            {
+                _merk = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public int TahunKeluar
+        {
+            get
+            {
+                return _tahunkeluar;
+            }
+            set
+            {
+                _tahunkeluar = value;
+            }
+        }
+
+        //method
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", _model, kecepatan);
+        }
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("suara klakson {0}", suara);
+        }
+
+        public void Tampilkaninfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merk {1}, model {2}, keluaran tahun {3}, dengan jumlah pintu {4}", _warna, _merk, _model, _tahunkeluar, _jumlahpintu);
+        }
+    }
+}
